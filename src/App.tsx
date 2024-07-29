6056c98dc9de01b9d5e1618af9e74279376c7f2e
Quan Gan <quan@ztag.com>
2024-07-29 14:38:17 +0800

    compiling but no output

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Editor from './pages/Editor';
 import DeckManagement from './pages/DeckManagement';
@@ -7,11 +7,11 @@ import DeckManagement from './pages/DeckManagement';
 const App: React.FC = () => {
   return (
     <div className="App">
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/editor" component={Editor} />
-        <Route path="/decks" component={DeckManagement} />
-      </Switch>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/editor" element={<Editor />} />
+        <Route path="/decks" element={<DeckManagement />} />
+      </Routes>
     </div>
   );
 };
